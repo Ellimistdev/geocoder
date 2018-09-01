7b802abb190be78dd1de62ab1e74aedfefb22533
Nick Rodriguez <endotnick@gmail.com>
2018-08-31 20:07:48 -0700

    implements request throttling

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const usingGoogle = true;
 const Geocoder = usingGoogle ? require('google-geocoder') : require('mapquest-geocoder');
 const { readFileSync, writeFileSync, createWriteStream } = require('fs');
+const pLimit = require('p-limit');
 
+const limit = pLimit(1);
 const keyPath = usingGoogle ? 'google.key' : 'mapquest5.key';
 const key = readFileSync(keyPath, 'utf8');
 const geo = usingGoogle ? Geocoder({ key }) : new Geocoder(key);
@@ -27,7 +29,6 @@ class Location {
       if (usingGoogle) {
         str += `,${this.place_id}`;
         this.address_components.forEach((el) => {
-        // for (const el of this.address_components) {
           str += `,${el.long_name}`;
         });
       } else {
@@ -70,23 +71,25 @@ function getUniqueLocations(data) {
     });
     resolve(entries);
   }).then((entries) => {
-    // split entries into sets of 5k
-    const sets = chunkArray(entries, 5000);
+    // split entries into sets of 100
+    const sets = chunkArray(entries, 100);
     let count = 0;
     sets.forEach((set) => {
-      writeFileSync(`unique${count}.json`, JSON.stringify(set, null, 2), { encoding: 'utf8' });
+      writeFileSync(`./locations/unique${count}.json`, JSON.stringify(set, null, 2), { encoding: 'utf8' });
       count += 1;
     });
   }).catch((error) => {
-    console.error(error);
+    throw error;
   });
 }
 
 function getInfo(coord) {
-  return new Promise((resolve) => {
-    const resolveRequest = (err, res) => {
-      if (err) throw err;
-      resolve(res);
+  return new Promise((resolve, reject) => {
+    const resolveRequest = (error, result) => {
+      if (error) {
+        reject(new Error('error resolving request: ', error));
+      }
+      resolve(result);
     };
     /* Google API */
     if (usingGoogle) {
@@ -95,61 +98,89 @@ function getInfo(coord) {
     /* Mapquest API */
       geo.geocode(coord, resolveRequest);
     }
+  }).catch((error) => {
+    throw error;
   });
 }
 function rebuildDates(entries, entry) {
   return entries[entry].timeStamps.map(timeStamp => new Date(timeStamp));
 }
 
-function geoCodeFromUnique(data) {
-  return new Promise((resolve) => {
+function geoCodeFromUnique(data, outFile) {
+  return new Promise((resolve, reject) => {
     Object.keys(data).forEach((index) => {
       data[index].timeStamps = rebuildDates(data, index);
     });
     resolve(data);
   }).then((entries) => {
-    const sharedheader = 'Date,Time,Latitude,Longitude,';
-    const gheader = `${sharedheader}Place Id,Bldg Number, Street, City, County, State, Country, Zip, Post Route\n`;
-    const mheader = `${sharedheader}Street, City, State, Country, Zip\n`;
-    const header = usingGoogle ? gheader : mheader;
-    const stream = createWriteStream('chrono.csv', { flags: 'a' });
-    stream.write(header);
-    const locs = [];
-    Object.keys(entries).forEach((index) => {      
-      const entry = Object.assign(new Location(`${entries[index].lat}, ${entries[index].long}`), entries[index]);
-      /*  Using Google API */
-      if (usingGoogle) {
-        getInfo(entry.coord) // billable $$$
+    // limit concurrent requests
+    limit(() => {
+      const sharedheader = 'Date,Time,Latitude,Longitude,';
+      const gheader = `${sharedheader}Place Id,Bldg Number, Street, City, County, State, Country, Zip, Post Route\n`;
+      const mheader = `${sharedheader}Street, City, State, Country, Zip\n`;
+      const header = usingGoogle ? gheader : mheader;
+      const stream = createWriteStream(`./results/chrono${outFile}.csv`, { flags: 'a' });
+      stream.write(header);
+      const locs = [];
+      Object.keys(entries).forEach((index) => {
+        const entry = Object.assign(new Location(`${entries[index].lat}, ${entries[index].long}`), entries[index]);
+        /*  Using Google API */
+        if (usingGoogle) {
+          getInfo(entry.coord) // billable $$$
+            .then((info) => {
+              entry.formatted_address = info[0].formatted_address;
+              entry.address_components = info[0].googleResponse.address_components;
+              entry.place_id = info[0].googleResponse.place_id;
+            })
+            .then(() => {
+              // append to stream
+              stream.write(entry.asChronologicalString());
+            })
+            .catch((error) => {
+              if (error.message.code !== 'TypeError') {
+                console.error('from getinfo:', error);
+              }
+            });
+        }
+        locs.push(entry.coord);
+      });
+      /* Using Mapquest API */
+      if (!usingGoogle) {
+        getInfo(locs) // billable $$$
           .then((info) => {
-            entry.formatted_address = info[0].formatted_address;
-            entry.address_components = info[0].googleResponse.address_components;
-            entry.place_id = info[0].googleResponse.place_id;
-          })
-          .then(() => {
-            // append to stream
-            stream.write(entry.asChronologicalString());
+            for (let i = 0; i < info.received.length; i += 1) {
+              const entry = Object.assign(new Location(`${entries[i].lat}, ${entries[i].long}`), entries[i]);
+              entry.address_components = info.received[i][0].locations[0];
+              stream.write(entry.asChronologicalString());
+            }
+          }).catch((error) => {
+            throw error;
           });
       }
-      locs.push(entry.coord);
+    }).catch((error) => {
+      console.error(error);
     });
-    /* Using Mapquest API */
-    if (!usingGoogle) {
-      getInfo(locs) // billable $$$
-        .then((info) => {
-          for (let i = 0; i < info.received.length; i += 1) {
-            const entry = Object.assign(new Location(`${entries[i].lat}, ${entries[i].long}`), entries[i]);
-            entry.address_components = info.received[i][0].locations[0];
-            stream.write(entry.asChronologicalString());
-          }
-          // return entries;
-        });
-    }
-  }).catch((error) => {
-    console.error(error);
   });
+}
+
+function timer(ms) {
+  return new Promise(res => setTimeout(res, ms));
+}
+
+async function run() {
+  const end = 594; // set val to last unique${val}.json file
+  let outFileIndex = 560; // set val to starting unique${val}.json file
+  for (let i = outFileIndex; i < end; i += 1) {
+    outFileIndex = i;
+    const inFile = JSON.parse(readFileSync(`./locations/unique${i}.json`, 'utf8'));
+    geoCodeFromUnique(inFile, outFileIndex);
+    console.log(outFileIndex);
+    await timer(10000); // rate limit, ~ 10 requests/sec
+  }
 }
 
 // given Location History.json, generate unique location files
 // getUniqueLocations(JSON.parse(readFileSync('Location History.json', 'utf8')));
+
 // given unique location files, generate csv of geocoded data
-geoCodeFromUnique(JSON.parse(readFileSync('unique0.json', 'utf8')));
+run();
